74ba300c59c44c5b0bc1a3e2a919c9e5935aabd0
Giovanna Martins <giovannamartins2604@gmail.com>
2026-05-05 00:38:40 -0300

    Correção
    Updated the password constant to an empty string.

--- util/conexao.java
+++ util/conexao.java
@@ -7,7 +7,7 @@ public class Conexao {
 
     private static final String URL     = "jdbc:mysql://localhost:3306/login_cleo?useSSL=false";
     private static final String USUARIO = "root";
-    private static final String SENHA   = "Laranjinha123@"; // altere para sua senha
+    private static final String SENHA   = ""; // altere para sua senha
 
     public static Connection conectar() throws Exception {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
